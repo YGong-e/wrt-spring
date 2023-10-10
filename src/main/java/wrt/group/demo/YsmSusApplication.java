a84b6acb3f98e6e65a65c95ade04775366e107fa
ysm <ysm40595@naver.com>
2023-10-10 16:37:11 +0900

    first-commit

---
+++ src/main/java/wrt/group/demo/YsmSusApplication.java
@@ -0,0 +1,13 @@
+package wrt.group.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsmSusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsmSusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wrt/group/demo/YsmSusApplicationTests.java
@@ -0,0 +1,13 @@
+package wrt.group.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsmSusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
