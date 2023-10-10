d6d5b6254d8aa955d7c6a541eac0df5749f3b2cb
ysm <ysm40595@naver.com>
2023-10-10 18:57:40 +0900

    message

--- src/test/java/wrt/group/demo/test/Test.java
+++ src/test/java/wrt/group/demo/test/Test.java
@@ -4,6 +4,6 @@ package wrt.group.demo.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("dddd");
+        System.out.println("dddddd");
     }
 }
